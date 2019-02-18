afbc75d56fc788617be6049ce913e13e12a9c0cc
jjgilbe3 <jjgilbe3@asu.edu>
2019-02-17 22:37:25 -0700

    Fixed the first bug reported on the chart, added file header

--- ReviewProject/src/main/java/Item.java
+++ ReviewProject/src/main/java/Item.java
@@ -1,3 +1,10 @@
+/*
+ * File: Item.java
+ * Author: Unknown author, amehlhase, jjgilbe3
+ * Date: February 17, 2019
+ * 
+ * Description: This file contains a class that has a constructor to set up a key
+*/
 package main.java;
 
 public final class Item {
